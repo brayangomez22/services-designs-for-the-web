2f8c951c2a054e629b4c0dd92a3d94e0820a219a
Brayan <gomezmancobrayanalexander@gmail.com>
2020-12-02 02:21:09 -0500

    first commit

---
+++ service-2/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(function () {
+        $('#section-feature .sf-wrap')
+            .hover(function() {
+                $(this).addClass('hover');
+            }, function() {
+                $(this).removeClass('hover');
+            })
+            .click(function(event) {
+                if (event.target.nodeName != 'A') {
+                    $(this).toggleClass('active');
+                }
+        });
+    });
+        
+});
